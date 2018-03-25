8772991e16cd3b9db7a6fc9873c6fedf70c83df4
Micheal Cole Young <coleyoungmusic@gmail.com>
2018-03-25 17:22:08 -0500

    Update TipCalculator.py

--- TipCalculator.py
+++ TipCalculator.py
@@ -7,25 +7,15 @@ def dollars_and_cents(x):
 
 
 def decrement(btn):
-    """
-    NOTE:
-    Bill total prints the correct format however the tip is still
-    returning a float.
-    """
     app.setEntry("Tip", app.getEntry("Tip") - 1)
     app.setLabel("Tip Total", app.getEntry("Tip") * app.getEntry("Bill") / 100)
-    app.setLabel("Bill Total", dollars_and_cents(app.getEntry("Bill") + app.getLabel("Tip Total")))
+    app.setLabel("Bill Total", app.getEntry("Bill") + app.getLabel("Tip Total"))
 
 
 def increment(btn):
-    """
-    NOTE:
-    Bill total prints the correct format however the tip is still
-    returning a float.
-    """
     app.setEntry("Tip", app.getEntry("Tip") + 1)
     app.setLabel("Tip Total", app.getEntry("Tip") * app.getEntry("Bill") / 100)
-    app.setLabel("Bill Total", dollars_and_cents(app.getEntry("Bill") + app.getLabel("Tip Total")))
+    app.setLabel("Bill Total", app.getEntry("Bill") + app.getLabel("Tip Total"))
 
 
 app = gui("Tip Calculator", "800x400")
@@ -43,8 +33,8 @@ app.setEntryAlign("Tip", "center")
 app.addButtons(["-", "+"], [decrement, increment])
 app.setButtonFont(20, font=None)
 app.addLabel("Amount to tip", "Tip Amount", 0, 2)
-app.addLabel("Tip Total", str(0), 1, 2)
+app.addLabel("Tip Total", 0, 1, 2)
 app.addLabel("Total", "Total", 2, 2)
-app.addLabel("Bill Total", str(0), 3, 2)
+app.addLabel("Bill Total", 0, 3, 2)
 
 app.go()
